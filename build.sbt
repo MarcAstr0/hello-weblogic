f3664e706759c9b06959148af22761d6e381e699
Mario Castro Squella <mario.castro.squella@gmail.com>
2017-10-16 16:06:04 -0300

    Converted Scala object to a Servlet in order to run it as a web application.

--- build.sbt
+++ build.sbt
@@ -3,4 +3,7 @@ name := "hello-weblogic"
 version := "0.1"
 
 scalaVersion := "2.12.3"
-        
+
+libraryDependencies += "javax.servlet" % "javax.servlet-api" % "3.0.1" % "provided"
+
+enablePlugins(TomcatPlugin)
--- src/main/scala/com/marcastr0/HelloWorld.scala
+++ src/main/scala/com/marcastr0/HelloWorld.scala
@@ -1,7 +1,13 @@
 package com.marcastr0
 
-object HelloWorld {
-  def main(args: Array[String]): Unit = {
-    println("Hello World!")
+import javax.servlet.http._
+
+class HelloWorldServlet extends HttpServlet {
+
+  override def doGet(request: HttpServletRequest, response: HttpServletResponse) {
+    response.setContentType("text/html")
+    response.setCharacterEncoding("UTF-8")
+    response.getWriter.write("""<h1>Hello, world!</h1>""")
   }
+
 }
